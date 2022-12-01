fc42e4d974ac1e357ad49dd2f08b1d0270ee1e4c
joaofguedes <up202108711@up.pt>
2022-12-01 21:16:47 +0000

    EX2 - Complete + .txt files and .epub conversion

--- EX2/txt2epub.c
+++ EX2/txt2epub.c
@@ -1,37 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/syscall.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
-int main(int argc, char* argv[]){
-    
-    
-    for(int i = 0;i < argc - 1; i++ ) //Criar procesos
-    {
-        createProcesses(argc -1);
-    }
 
-    for(int i = 0;i < argc - 1; i++ ) //Fechar procesos
-    {
-        wait(NULL);
-    }
+void converter(char *f)
+{
+  char *fDuplicate = strdup(f);
+  int len = strlen(fDuplicate);
+  fDuplicate[len - 4] = '\0';
+  strcat(fDuplicate, ".epub");
 
-    system("zip ebooks *.epub");
-
+  execlp("pandoc", "pandoc", f, "-o", fDuplicate, "--quiet", (char *)NULL);
 }
 
 
-void converter(f){
-    for (int i = 0; i < argc-1; i++){
-    exelp("pandoc", "pandoc", f, "-o", file.epub, (char*)NULL);
-    }
+void createProcesses(char *f)
+{
+  int pid;
+
+  if ((pid = fork()) < 0) // Pegar no Child Process
+  {
+    printf("[%d] converting %s \n", pid, f);
+    perror("fork: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid == 0) // Child Process Executa o Programa
+  {
+    converter(f);
+    perror("execlp: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid >= 1) // Parent Process Espera
+  {
+    printf("[pid%d] converting %s \n", pid, f);
+  }
 }
 
-void createProcesses(n){
-        
-        if(fork() == 0)
-        {
-            converter(argv[n+1]);
-            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
-        }
+int main(int argc, char *argv[])
+{
+  for (int i = 0; i < argc - 1; i++) // Criar procesos
+  {
+    createProcesses(argv[i + 1]);
+  }
 
+  for (int i = 0; i < argc; i++) // Fechar procesos
+  {
+    wait(NULL);
+  }
+
+  system("zip ebooks *.epub");
+  return (1);
 }
+
